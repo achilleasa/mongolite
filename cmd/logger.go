7b15de3f8d757b81f030e5835cf9ee617eaab06d
Achilleas Anagnostopoulos <achilleas.imap.mbox@gmail.com>
2020-03-14 10:38:33 +0000

    Move cli command implementations in the cmd package

---
+++ cmd/logger.go
@@ -0,0 +1,25 @@
+package cmd
+
+import (
+	"os"
+
+	"gopkg.in/Sirupsen/logrus.v1"
+	"gopkg.in/urfave/cli.v2"
+)
+
+var (
+	rootLogger = logrus.New()
+	appLogger  = rootLogger.WithField("module", "app")
+)
+
+// SetupLogger is invoked by the cli before a command is executed.
+func SetupLogger(*cli.Context) error {
+	rootLogger.SetOutput(os.Stderr)
+	return nil
+}
+
+// ExitErrorHandler is invoked when the cli encounters a fatal error.
+func ExitErrorHandler(_ *cli.Context, err error) {
+	appLogger.WithError(err).Errorf("terminating due to error")
+	os.Exit(1)
+}
