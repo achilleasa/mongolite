c2128de6858daad67251396cd682bf0f2a25bb8e
Achilleas Anagnostopoulos <achilleas.imap.mbox@gmail.com>
2020-03-25 08:05:58 +0000

    Extend RequestHandler to include a clientID and a RemoveClient methd
    These changes allows the handlers to be aware of the client making the
    request and ensures that we can properly cleanup any internal state
    (e.g. cursor state in the future) when clients disconnect.

--- proxy/handler.go
+++ proxy/handler.go
@@ -7,14 +7,8 @@ import "io"
 type RequestHandler interface {
 	// HandleRequest processes the incoming request and writes a response
 	// using the mongo wire format back to the provided io.Writer.
-	HandleRequest(io.Writer, []byte) error
-}
+	HandleRequest(clientID string, w io.Writer, r []byte) error
 
-// RequestHandlerFunc is an adaptor for converting a function with a suitable
-// signature into a RequestHandler.
-type RequestHandlerFunc func(io.Writer, []byte) error
-
-// HandleRequest invokes the wrapped function with the provided arguments.
-func (f RequestHandlerFunc) HandleRequest(w io.Writer, r []byte) error {
-	return f(w, r)
+	// RemoveClient is invoked when the remote mongo client disconnects.
+	RemoveClient(clientID string) error
 }
