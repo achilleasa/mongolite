126e86698833c044be765a407d17114503ed7b20
Achilleas Anagnostopoulos <achilleas.imap.mbox@gmail.com>
2020-03-31 08:43:54 +0100

    Define generic response payload and implement a wire encoder for it

---
+++ protocol/response.go
@@ -0,0 +1,26 @@
+package protocol
+
+import (
+	"gopkg.in/mgo.v2/bson"
+)
+
+// ResponseFlag represents the allowed flag values for a reply message.
+type ResponseFlag uint32
+
+const (
+	// Set when getMore is called but the cursor id is not valid at the
+	// server.
+	ResponseFlagCursorNotFound ResponseFlag = 1 << iota
+
+	// Set when a query failed. The reply will include a single document
+	// with more details about the error.
+	ResponseFlagQueryError
+)
+
+// Response represents a response to a mongo client request.
+type Response struct {
+	Flags        ResponseFlag
+	CursorID     int64
+	StartingFrom int32
+	Documents    []bson.M
+}
